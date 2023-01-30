6235ec18956f9475cbc9100c18e3ed1141ea2d46
n_v_e_younes <102386293+nveXyounes@users.noreply.github.com>
2023-01-30 11:19:04 +0100

    update

---
+++ C (Examen)/exercice_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[100];
+    int spaces = 0, alphabets = 0, numbers = 0, i;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    for (i = 0; str[i]; i++) {
+        if (isspace(str[i])) {
+            spaces++;
+        } else if (isalpha(str[i])) {
+            alphabets++;
+        } else if (isdigit(str[i])) {
+            numbers++;
+        }
+    }
+
+    printf("Spaces: %d\nAlphabets: %d\nNumbers: %d\n", spaces, alphabets, numbers);
+
+    return 0;
+}
