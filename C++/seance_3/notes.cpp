1a260b287d8507d140b71369244268a13fe5d22b
n_v_e_younes <102386293+nveXyounes@users.noreply.github.com>
2023-03-15 16:47:48 +0100

    Master

--- C++/seance_3/notes.cpp
+++ C++/seance_3/notes.cpp
@@ -2,12 +2,47 @@
 #include <iomanip>
 
  using namespace std;
+   
+  class produit{
+
+    int ref,red;
+    string titre;
+    float prix;
+      
+    double calculeprix()
+     {
+      return prix*(1-(double)red/100);
+    }
+
+    public:
+    void create()
+     {
+      cout<<"enter les informations "<<endl;
+      cin>>ref>>red>>titre>>prix;
+    }
+
+     void show(){
+     }
+   };
+        void produit::show()
+     {
+      cout<<ref<<" - "<<red<<" - "<<titre<" - "<<prix<<" DHS vendu a "<<calculeprix()<<" DHS "<<endl;
+    }
+
 
   int main(){
+     
+  produit p1,p2;
+
+  p1.create();
+  p1.show();
+  p2.show();
+
 /*
 notion orienté objet
 expression instance, classe, les attributs
 Encapsulation
+Public-Private-Protected
 */
     return 0;
   }
